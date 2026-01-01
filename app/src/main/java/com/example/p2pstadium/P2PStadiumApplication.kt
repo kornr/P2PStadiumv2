c4e87d9f74dcf09102803fe638decdadee5d22bd
kornr <infotecnova6@gmail.com>
2026-01-01 02:33:34 +0000

    Afegit anàlisi de xarxa i sniffer per a AP: detecta clients connectats i mostra GPS

---
+++ app/src/main/java/com/example/p2pstadium/P2PStadiumApplication.kt
@@ -0,0 +1,10 @@
+package com.example.p2pstadium
+
+import android.app.Application
+
+class P2PStadiumApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Inicialització addicional (si cal en el futur)
+    }
+}
